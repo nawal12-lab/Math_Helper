7f0dac518fb5c31fe501ff5cf76a9af7587c33f4
Eng Saqaawe <nuurc9399@gmail.com>
2025-07-09 16:36:08 +0300

    me

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template,
+app = Flask (__name__)
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/templates/addition.html')
+def addition():
+    return render_template('addition.html')
+@app.route('/templates/multiplication.html')
+def multiplication():
+    return render_template('multiplication.html')
+@app.route('/templates/subtract.html')
+def subtract():
+    return render_template('subtract.html')
+@app.route('/templates/divide.html')
+def divide():
+    return render_template('divide.html')
+@app.route('/templates/sqrt.html')
+def sqrt():
+    return render_template('sqrt.html')
+if __name__ == ('__main__'):
+    app.run(debug=True)
